4ed415eba2a260c18a9971e82fc7b49f94eb6cb6
Ana Médlyn Galhardo <134453403+AnaMedlyn@users.noreply.github.com>
2023-09-15 08:40:33 -0300

    Add files via upload

---
+++ Ex de Manipulação de Arquivos/criar arquivo.py
@@ -0,0 +1,7 @@
+
+arquivo = open("C:\\Users\\Instrutor\\Desktop\\teste\\testando.txt", "w")
+
+arquivo.write("Olá, Mundo!\n")
+arquivo.write("Este é um exemplo de escrito em arquivo.\n")
+
+arquivo.close()
---
+++ Ex de Manipulação de Arquivos/leitura arquivo.py
@@ -0,0 +1,7 @@
+arquivo = open("C:\\Users\\Instrutor\\Desktop\\teste\\testando.txt", "r")
+
+conteudo = arquivo.read()
+
+arquivo.close()
+
+print(conteudo)
