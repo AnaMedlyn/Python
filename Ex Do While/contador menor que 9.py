4a20f099b71291a1f5bae9aacd5b6e155758a068
Ana Médlyn Galhardo <134453403+AnaMedlyn@users.noreply.github.com>
2023-09-15 08:40:48 -0300

    Add files via upload

---
+++ Ex Do While/contador menor que 9.py
@@ -0,0 +1,13 @@
+contador = 9
+
+while contador > -1:
+    print(contador)
+    contador -= 1
+
+#OU
+    
+contador = 9
+
+while contador >= 0:
+    print(contador, end = " ") #assim o resultado vai aparecer horizontal (mesma linha)
+    contador -= 1
---
+++ Ex Do While/contador.py
@@ -0,0 +1,9 @@
+numero = int(input("Digita ae"))
+
+contador = 1
+resultado= 1
+
+while (contador <= numero):
+    resultado *= contador
+    contador += 1
+print(resultado)
